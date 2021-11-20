db8497d5ccd2e2f39ee121ae1144229f0ed1db10
benjaakpe <54922375+benjaakpe@users.noreply.github.com>
2021-11-20 13:03:40 -0600

    Update models.py

--- Sandaapp/models.py
+++ Sandaapp/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-
-# Create your models here.
-# Create your models here.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
@@ -56,15 +53,15 @@ class Customer(models.Model):
         return str(self.user.username)
 
 
-# @receiver(post_save, sender=User)
-# def create_user_customer(sender, instance, created, **kwargs):
-#     if created:
-#         Customer.objects.create(user=instance)
-#
-#
-# @receiver(post_save, sender=User)
-# def save_user_customer(sender, instance, **kwargs):
-#     instance.customer.save()
+@receiver(post_save, sender=User)
+def create_user_customer(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_customer(sender, instance, **kwargs):
+    instance.customer.save()
 
 
 # category table
